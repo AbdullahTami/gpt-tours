d596298d84451470410148e73d8bcde004fa4fcf
abdullahqayid <3boo0odq0v@gmail.com>
2024-02-24 01:45:12 +0300

    messages successfuly rendered

--- components/Chat.jsx
+++ components/Chat.jsx
@@ -3,25 +3,50 @@
 import { generateChatResponse } from "@/utlis/actions";
 import { useMutation } from "@tanstack/react-query";
 import { useState } from "react";
+import toast from "react-hot-toast";
 
 function Chat() {
   const [text, setText] = useState("");
   const [messages, setMessages] = useState([]);
 
-  const { mutate: createMessage } = useMutation({
-    mutationFn: (message) => generateChatResponse(message),
+  const { mutate: createMessage, isPending } = useMutation({
+    mutationFn: (query) => generateChatResponse([...messages, query]),
+    onSuccess: (data) => {
+      if (!data) {
+        toast.error("Something went wrong");
+        return;
+      }
+      setMessages((messages) => [...messages, data]);
+    },
   });
 
   function handleSubmit(e) {
-    if (!text) return;
     e.preventDefault();
-    createMessage(text);
+    if (!text) return;
+    const query = { role: "user", content: text };
+    createMessage(query);
+    setMessages((messages) => [...messages, query]);
     setText("");
   }
+  console.log(messages);
+
   return (
     <div className="min-h-[calc(100vh-6rem)] grid grid-rows-[1fr,auto]">
       <div>
-        <h2 className="text-5xl">messages</h2>
+        {messages.map(({ role, content }, index) => {
+          const avatar = role === "user" ? "🧑‍💻" : "🤖";
+          const bcg = role === "user" ? "bg-base-200" : "bg-base-100";
+          return (
+            <div
+              key={index}
+              className={`${bcg} flex py-6 -mx-8 px-8 text-xl leading-loose border-b border-base-300`}
+            >
+              <span className="mr-4">{avatar}</span>
+              <p className="max-w-3xl">{content}</p>
+            </div>
+          );
+        })}
+        {isPending && <span className="loading"></span>}
       </div>
 
       <div>
@@ -35,8 +60,12 @@ function Chat() {
               placeholder="Message GPTTours"
               className="input input-bordered join-item w-full"
             />
-            <button className="btn btn-primary join-item" type="submit">
-              Ask question
+            <button
+              disabled={isPending}
+              className="btn btn-primary join-item"
+              type="submit"
+            >
+              {isPending ? "Please wait" : "Ask question"}
             </button>
           </div>
         </form>
--- utlis/actions.js
+++ utlis/actions.js
@@ -6,23 +6,23 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-export async function generateChatResponse(chatMessage) {
-  //   console.log(chatMessage);
-  const response = await openai.chat.completions.create({
-    messages: [
-      {
-        role: "system",
-        content: "you are a helpful assistant",
-      },
-      {
-        role: "user",
-        content: chatMessage,
-      },
-    ],
-    model: "gpt-3.5-turbo",
-    temperature: 0,
-  });
-  console.log(response.choices[0].message);
-  console.log(response);
-  return "awesome";
+export async function generateChatResponse(chatMessages) {
+  //   console.log(chatMessages);
+  try {
+    const response = await openai.chat.completions.create({
+      messages: [
+        {
+          role: "system",
+          content: "you are a helpful assistant",
+        },
+        ...chatMessages,
+      ],
+      model: "gpt-3.5-turbo",
+      temperature: 0,
+    });
+    return response.choices[0].message;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
 }
